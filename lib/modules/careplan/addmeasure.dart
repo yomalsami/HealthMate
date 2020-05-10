252d4d41c3b30c82b2d2e2642391023c644867c1
Manav Modi <55242552+manavmodi22@users.noreply.github.com>
2020-05-10 11:35:10 +0530

    10/05

--- lib/modules/careplan/addmeasure.dart
+++ lib/modules/careplan/addmeasure.dart
@@ -11,22 +11,94 @@ class vitalHomePage extends StatefulWidget {
 class _vitalHomePageState extends State<vitalHomePage> {
   @override
   Widget build(BuildContext context) {
+    var _value;
     return Scaffold(
       backgroundColor: Colors.black,
       floatingActionButton: FAB2(),
       floatingActionButtonLocation: _CustomStartTopFloatingActionButtonLocation(),
       appBar: MyAppbar(),
       body: SingleChildScrollView(
+
         child:new Column(
           children: <Widget>[
             new Padding(padding: EdgeInsets.only(top: 30.0)),
             new HomePageBody(),
-          ],
+            new Stack(
+              children: [
+                CustomSliderThumbCircle(thumbRadius: 5.0),
+              ],
+            )
+            ],
         )
       ),
     );
   }
 }
+
+class CustomSliderThumbCircle extends SliderComponentShape {
+  final double thumbRadius;
+  final int min;
+  final int max;
+
+  const CustomSliderThumbCircle({
+    @required this.thumbRadius,
+    this.min = 0,
+    this.max = 10,
+  });
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return Size.fromRadius(thumbRadius);
+  }
+
+  @override
+  void paint(PaintingContext context,
+      Offset center,
+      {Animation<double> activationAnimation,
+        Animation<double> enableAnimation,
+        bool isDiscrete,
+        TextPainter labelPainter,
+        RenderBox parentBox,
+        SliderThemeData sliderTheme,
+        TextDirection textDirection,
+        double value,
+        double textScaleFactor,
+        Size sizeWithOverflow}) {
+    final Canvas canvas = context.canvas;
+    final paint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+    TextSpan span = new TextSpan(
+      style: new TextStyle(
+        fontSize: thumbRadius * .8,
+        fontWeight: FontWeight.w700,
+        color: sliderTheme.thumbColor,
+      ),
+      text: getValue(value),
+    );
+    TextPainter tp = new TextPainter(
+        text: span,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr
+    );
+    tp.layout();
+    Offset textCenter =
+    Offset(center.dx - (tp.width / 2), center.dy - (tp.height / 2));
+
+    canvas.drawCircle(center, thumbRadius * .9, paint);
+    tp.paint(canvas, textCenter);
+  }
+
+  String getValue(double value) {
+    return ((max * value).round()).toString();
+  }
+}
+
+
+
+
+
+
 
 
 
