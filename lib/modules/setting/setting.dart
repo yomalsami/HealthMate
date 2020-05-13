7ebe2b03cf94a1b3ebce0498262f746b72ffcdc1
Manav Modi <55242552+manavmodi22@users.noreply.github.com>
2020-05-13 12:02:58 +0530

    13/05/2020(12:02)
    Sharing fuction and settings page added

--- lib/modules/setting/setting.dart
+++ lib/modules/setting/setting.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:share/share.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -30,11 +32,173 @@ class _SettingsState extends State<Settings> {
           Padding(padding: EdgeInsets.only(left:50.0))
         ],
         ),
-        body: SingleChildScrollView(
-        ),
 
+        body:SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top:30.0),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10.0)),
+                margin:const EdgeInsets.all(8.0),
+                color:Colors.teal,
+                child:Column(
+                  children: <Widget>[
+                    ListTile(
+                      onTap:(){},
+                      leading: Icon(
+                        LineAwesomeIcons.users,
+                        color: Colors.white,
+                        size:30.0
+                      ),
+                      title:Text("My Care Team",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                      trailing: Icon(
+                        LineAwesomeIcons.arrow_right,
+                        color:Colors.white,
+                        size: 30.0,
+                      ),
+                    ),
+                    Divider(
+                      height:10.0,
+                      thickness: 5.0,
+                      color: Colors.black,
+                    ),
+                    ListTile(
+                      onTap:(){},
+                      leading: Icon(
+                        Icons.book,
+                        color: Colors.white,
+                        size:30.0
+                      ),
+                      trailing: Icon(
+                        LineAwesomeIcons.arrow_right,
+                        color:Colors.white,
+                        size: 30.0,
+                      ),
+                      title:Text("My Care Plan",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                    )
+                    
+                  ],
+                )
+              ),
+              // SizedBox(
+              //   height:10.0,
+              //   width:200.0,
+              // ),
+              Card(
+                shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10.0)),
+                margin:const EdgeInsets.all(8.0),
+                color:Colors.teal,
+                child:Column(
+                  children: [
+                    // ListTile(
+                    //   onTap:(){},
+                    //   title:Text("Health Integrations",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400)),
+                    // ),
+                    // Divider(
+                    //   height:10.0,
+                    //   thickness: 5.0,
+                    //   color: Colors.black,
+                    // ),
+                    ListTile(
+                      onTap:(){},
+                      leading: Icon(
+                        LineAwesomeIcons.cog,
+                        color: Colors.white,
+                        size:30.0
+                      ),
+                      
+                      title:Text("User Settings",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                    ),
+                    Divider(
+                      height:10.0,
+                      thickness: 5.0,
+                      color: Colors.black,
+                    ),
+                    ListTile(
+                      onTap:(){},
+                      leading: Icon(
+                        LineAwesomeIcons.info,
+                        color: Colors.white,
+                        size:30.0
+                      ),
+                      title:Text("Resource Center",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                    ),
+                    Divider(
+                      height:10.0,
+                      thickness: 5.0,
+                      color: Colors.black,
+                    ),
+                    ListTile(
+                      onTap:(){
+                        Share.share('Check out this cool new app!');
+                      },
+                      leading: Icon(
+                        LineAwesomeIcons.share_alt,
+                        color: Colors.white,
+                        size:30.0
+                      ),
+                      title:Text("Share App with Friends",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                    ),
 
-
-        );
+                  ],
+                )
+              ),
+              Card(
+                shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10.0)),
+                margin:const EdgeInsets.all(8.0),
+                color:Colors.teal,
+                child:Column(
+                  children: [
+                    ListTile(
+                      onTap:(){},
+                      leading: Icon(
+                        LineAwesomeIcons.shield,
+                        color: Colors.white,
+                        size:30.0
+                      ),
+                      title:Text("Privacy Policy",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                    ),
+                    Divider(
+                      height:10.0,
+                      thickness: 5.0,
+                      color: Colors.black,
+                    ),
+                    ListTile(
+                      onTap:(){},
+                      leading: Icon(
+                        LineAwesomeIcons.play,
+                        color: Colors.white,
+                        size:30.0
+                      ),
+                      trailing: Text("1.0",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                      title:Text("App Version",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                    ),
+                  ],
+                )
+              ),
+              SizedBox(
+                height:50.0,
+                width:200.0,
+              ),
+              Card(
+                shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10.0)),
+                margin:const EdgeInsets.all(8.0),
+                color:Colors.teal,
+                child:ListTile(
+                  onTap:(){},
+                  leading: Icon(
+                        LineAwesomeIcons.sign_out,
+                        color: Colors.white,
+                        size:30.0
+                      ),
+                  title:Text("Logout",style:TextStyle(color:Colors.white,fontWeight:FontWeight.w400,fontSize: 20.0)),
+                )
+              ),
+            ],
+          ),
+        )
+      );
   }
 }
